f33d44bbd17a2ae920a5d1300a9b2fedec2de3cb
chentianming002 <tm19930109>
2021-03-10 16:34:17 +0800

    test

--- src/main/java/com/chen/making/wheel/demo/aspect/LogAspect.java
+++ src/main/java/com/chen/making/wheel/demo/aspect/LogAspect.java
@@ -10,6 +10,8 @@ public class LogAspect {
     public void before(){
         //往对象里面记录调用的开始时间
         System.out.println("开始执行：start => " + System.currentTimeMillis());
+
+        // 测试github提交代码
     }
 
     public void afterReturning(){
